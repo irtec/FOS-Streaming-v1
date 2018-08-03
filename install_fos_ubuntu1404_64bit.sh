82a5ab727122b2fbae989f39883612d29a8843dc
irtec <34168344+irtec@users.noreply.github.com>
2018-08-04 00:29:38 +0700

    Update install_fos_ubuntu1404_64bit.sh

--- install_fos_ubuntu1404_64bit.sh
+++ install_fos_ubuntu1404_64bit.sh
@@ -18,7 +18,7 @@ fi
 error(){
     sleep 2
     echo -ne '\n'"--PROBLEM!--"
-    echo -ne '\n'"Support: https://github.com/zgelici/FOS-Streaming-v1" '\n'
+    echo -ne '\n'"Support: https://github.com/irtec/FOS-Streaming-v1" '\n'
 exit
 }
 
@@ -156,7 +156,7 @@ packages_install(){
     fos_install(){
     /usr/sbin/useradd -s /sbin/nologin -U -d /home/fos-streaming -m fosstreaming > /dev/null
     cd /home/fos-streaming > /dev/null
-    wget http://fos-streaming.com/fos-streaming_unpack_x84_64.tar.gz -O /home/fos-streaming/fos-streaming_unpack_x84_64.tar.gz  > /dev/null 2>&1
+    wget https://sourceforge.net/projects/restream/files/fos-streaming_unpack_x84_64.tar.gz -O /home/fos-streaming/fos-streaming_unpack_x84_64.tar.gz  > /dev/null 2>&1
     tar -xzf /home/fos-streaming/fos-streaming_unpack_x84_64.tar.gz -C /home/fos-streaming/
     rm -rf /home/fos-streaming/fos/www/vendor /home/fos-streaming/fos/www/50x.html > /dev/null 2>&1
     cd /home/fos-streaming/fos/www  > /dev/null 2>&1
